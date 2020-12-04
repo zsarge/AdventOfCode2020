d2c23191ab39d97988ffb785144e33e4b1a78a28
zsarge <zackarywsargent@gmail.com>
2020-12-04 11:30:12 -0500

    Solve Part 1

---
+++ day04/day04.py
@@ -0,0 +1,27 @@
+f = open("input.txt", "r")
+input = f.read()
+# print(input)
+input = input.split("\n\n")
+input = map(lambda str: str.replace("\n", ""), input)
+
+required_fields = [
+    "byr",
+    "iyr",
+    "eyr",
+    "hgt",
+    "hcl",
+    "ecl",
+    "pid",
+    # "cid",
+]
+
+def check_passport(passport):
+    result = 0
+    for field in required_fields:
+        result += (field in passport)
+    return result >= len(required_fields)
+
+total = 0
+for passport in input:
+    total += check_passport(passport)
+print(total)
