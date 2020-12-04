05b4c8e8e3df90c12faf9955382a1a3ff23e1531
zsarge <zackarywsargent@gmail.com>
2020-12-04 16:41:37 -0500

    Solve day 4 part 2

--- day04/day04_part2.py
+++ day04/day04_part2.py
@@ -32,7 +32,7 @@ def valid_eyr(content: str) -> bool:
 def valid_hgt(content: str) -> bool:
     if content[-2:] == "cm":
         return test_string(content[:-2], 150, 193)
-    if content[-2:] == "cm":
+    if content[-2:] == "in":
         return test_string(content[:-2], 59, 76)
     return False
 
@@ -46,12 +46,12 @@ def valid_ecl(content: str) -> bool:
     return (total == 1)
 
 def valid_pid(content: str) -> bool:
-        return (len(content) == 9 and content.isdigit())
+    return (len(content) == 9 and content.isdigit())
 
-def valid_cid(content: str) -> False:
-    return False
+def valid_cid(content: str) -> True:
+    return True
 
-required_fields = {
+all_fields = {
     "byr": valid_byr,
     "iyr": valid_iyr,
     "eyr": valid_eyr,
@@ -62,32 +62,61 @@ required_fields = {
     "cid": valid_cid
 }
 
+# all_fields[tag](field[4:])
+def valid_field(name: str, content: str) -> bool:
+    return all_fields[name](content)
+
+required_fields = [
+    "byr",
+    "iyr",
+    "eyr",
+    "hgt",
+    "hcl",
+    "ecl",
+    "pid",
+]
+
 total = 0
 for passport in input:
-    pass_fields = passport.split(" ")
+    passport_fields = passport.split(" ")
     print(f"\n{passport}")
 
-    valid_fields = 0
-    for field in pass_fields:
-        if len(field) <= 0:
-            break
+    valid_fields = []
+    for field in passport_fields:
         tag = field[:3]
-        print(f"Tag: {tag} - {required_fields[tag](field[4:])}")
-        if required_fields[tag](field[4:]):
-            valid_fields += 1
+        print(f"Tag: {tag} - Content: {field[4:]}\t- {valid_field(tag, field[4:])}")
+        valid_fields.append(valid_field(tag, field[4:]))
             
-    unique_fields = 0
+    print()
+    unique_fields = []
     for field in required_fields:
-        unique_fields += (field in passport)
+        result = False
+        if passport.count(field) == 1:
+            result = True 
+        unique_fields.append(result)
+        print(f"{field} in passport - {result}")
+    print()
 
     # decrement to account for cid
-    if (unique_fields >= (len(required_fields) - 1)) and (valid_fields >= (len(required_fields) - 1)):
+    # if (unique_fields >= (len(all_fields) - 1)) and (valid_fields >= (len(all_fields) - 1)):
+    # if (False not in unique_fields) and (False not in valid_fields):
+    if False not in valid_fields and False not in unique_fields:
         print("Entry marked as valid")
         total += 1
+    else:
+        print("NOT VALID")
+        print(f"False not in unique_fields: {False not in unique_fields} - False not in valid_fields: {False not in valid_fields}")
+    print("\n------")
     
 print()
 print("total")
 print(total)
+if total >= 182 or total <= 19 or total == 49 or total == 108 or total == 145:
+    print("WRONG ANSWER")
+
+# 190 is too high
 # 182 is too high
-# 108
-# 19 is too low
+# 108 is also wrong
+# 145 is also wrong
+#  49 is also wrong
+#  19 is too low
